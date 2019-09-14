4651a1cd8aef0e3ae55322cf2fae80caa79195c1
Evgeniy <timeinvest27@gmail.com>
2019-09-14 18:38:54 +0300

    ...

--- ChatServerConsoleSimberSoft/Models/User.cs
+++ ChatServerConsoleSimberSoft/Models/User.cs
@@ -8,5 +8,11 @@ namespace ChatServerConsoleSimberSoft.Models
 {
     class User
     {
+        public string ConnectionId { get; set; }
+        public string Login { get; set; }
+
+        //public string Password { get; set; }
+        //public bool Moderator { get; set; }
+        //public bool Locked { get; set; }
     }
 }
