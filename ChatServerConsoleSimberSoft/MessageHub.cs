14c79962fb6d2b3c98b83b9b6e0f0a13d4cc4421
Evgeniy <timeinvest27@gmail.com>
2019-09-15 20:19:38 +0300

    Список сообщений

--- ChatServerConsoleSimberSoft/MessageHub.cs
+++ ChatServerConsoleSimberSoft/MessageHub.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNet.SignalR;
+﻿using ChatServerConsoleSimberSoft.Models;
+using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
 using System;
 using System.Collections.Generic;
@@ -11,12 +12,38 @@ namespace ChatServerConsoleSimberSoft
     [HubName("MessageHub")]
     public class MessageHub : Hub
     {
+        static List<User> Users = new List<User>();
+        static List<Message> Messages = new List<Message>();
+
         public void SendMessage(string user, string message)
         {
             Console.WriteLine(message);
 
             string FullMessage = string.Format(user + ": " + message);
             Clients.All.ReceiveLength(FullMessage);
+
+            Messages.Add(new Message { Text = FullMessage, Time = DateTime.Now, UserName = user });
+
+            // Сначала надо реализовать добавление пользователей.
+            //Messages.Add(new Message { Text = message, Time = DateTime.Now, User = Users.Single(x => x.Login == user) });
+
+        }
+
+        public void Connect(string userName)
+        {
+            var id = Context.ConnectionId;
+
+
+            if (!Users.Any(x => x.ConnectionId == id))
+            {
+                Users.Add(new User { ConnectionId = id, Login = userName });
+
+                // Посылаем сообщение текущему пользователю
+                Clients.Caller.onConnected(id, userName, Users);
+
+                // Посылаем сообщение всем пользователям, кроме текущего
+                Clients.AllExcept(id).onNewUserConnected(id, userName);
+            }
         }
     }
 }
--- ChatServerConsoleSimberSoft/Models/Message.cs
+++ ChatServerConsoleSimberSoft/Models/Message.cs
@@ -9,6 +9,7 @@ namespace ChatServerConsoleSimberSoft.Models
     class Message
     {
         public string Text { get; set; }
+        public string UserName { get; set; }
         public User User { get; set; }
         public DateTime Time { get; set; }
     }
